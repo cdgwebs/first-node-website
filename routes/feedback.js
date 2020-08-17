6a1e9bad11e570e2393a0f4c0ae9e76867cbb79e
David M Graham <d5graha@gmail.com>
2020-08-16 21:32:19 -0500

    Last commit, check out advanced express

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,8 +1,32 @@
 const express = require('express');
 
 const { check, validationResult } = require('express-validator');
+const { request, response } = require('express');
 
 const router = express.Router();
+
+const validations = [
+    check('name')
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('A name is required'),
+    check('email')
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage('An email address is required'),
+    check('title')
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage('A title is required'),
+    check('message')
+    .trim()
+    .isLength({ min: 5 })
+    .escape()
+    .withMessage('A message is required'),
+];
 
 /*  This is really cool using the arrow function because
     you can pass params with this module.export
@@ -33,30 +57,8 @@ module.exports = (params) => {
         }
     });
 
-    router.post(
-        '/', [
-            check('name')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A name is required'),
-            check('email')
-            .trim()
-            .isEmail()
-            .normalizeEmail()
-            .withMessage('An email address is required'),
-            check('title')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A title is required'),
-            check('message')
-            .trim()
-            .isLength({ min: 5 })
-            .escape()
-            .withMessage('A message is required'),
-        ],
-        async(request, response) => {
+    router.post('/', validations, async(request, response, next) => {
+        try {
             const errors = validationResult(request);
             if (!errors.isEmpty()) {
                 request.session.feedback = {
@@ -70,8 +72,29 @@ module.exports = (params) => {
                 message: 'Thank you for your feedback!',
             };
             return response.redirect('/feedback');
+        } catch (err) {
+            return next(err);
         }
-    );
+    });
+
+    router.post('/api', validations, async(request, response, next) => {
+        try {
+            const errors = validationResult(request);
+            if (!errors.isEmpty()) {
+                return response.json({ errors: errors.array() });
+            }
+            const { name, email, title, message } = request.body;
+            await feedbackService.addEntry(name, email, title, message);
+            const feedback = await feedbackService.getList();
+
+            return response.json({
+                feedback,
+                successMessage: 'Thank you for your feedback!',
+            });
+        } catch (err) {
+            return next(err);
+        }
+    });
 
     return router;
 };
