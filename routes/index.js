e319d357f7854ab6e11c495b78b8c65ed0274a89
David M Graham <d5graha@gmail.com>
2020-08-21 15:33:04 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,12 @@ module.exports = (params) => {
             const topSpeakers = await speakerService.getList();
             const allArtwork = await speakerService.getAllArtwork();
             /* // Can be used for storing session variables
-                      if (!request.session.visitCount) {
-                          request.session.visitCount = 0;
-                      }
-                      request.session.visitCount += 1;
-                      console.log(`Visit count is: ${request.session.visitCount}`);
-                  */
+                                  if (!request.session.visitCount) {
+                                      request.session.visitCount = 0;
+                                  }
+                                  request.session.visitCount += 1;
+                                  console.log(`Visit count is: ${request.session.visitCount}`);
+                              */
             return response.render('layout', {
                 pageTitle: 'Welcome',
                 template: 'index',
